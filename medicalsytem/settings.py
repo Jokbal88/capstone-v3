441e65cee31a3c08de49867cdbc11d78b06c63a9
Sherwin Romano <sherwinjayromano@gmail.com>
2025-06-09 09:35:54 +0800

    set ssl to true

--- medicalsytem/settings.py
+++ medicalsytem/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
         default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
         conn_max_age=600,
         conn_health_checks=True,
-        # ssl_require=True
+        ssl_require=True
     )
 }
 
